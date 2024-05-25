d9a4f161b1e3da5b41516e0dae5b00c1a3bc5119
CodingBeagle <kasper.rie@gmail.com>
2024-05-25 16:26:20 +0200

    creating a window

---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <Windows.h>
+#include <string>
+#include <iostream>
+
+// Forward Decl
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+void RedirectIOToConsole()
+{
+    // Allocate a console window
+    AllocConsole();
+
+    // Redirect the standard input, output, and error handles to the console window
+    freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
+    freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+    freopen_s((FILE**)stderr, "CONOUT$", "w", stderr);
+
+    // Clear the error state for each of the C++ standard stream objects. We need to do this, as C++ standard streams are tied to the standard C streams and this is the reason why we need to flush the C++ streams as well.
+    std::wcout.clear();
+    std::cout.clear();
+    std::wcerr.clear();
+    std::cerr.clear();
+    std::wcin.clear();
+    std::cin.clear();        
+}
+
+// Windows Desktop Applications have a WinMain function as the entrypoint.
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
+    RedirectIOToConsole();
+
+    WNDCLASSEX wcex = {};
+
+    std::string window_class_name = "Main Game Window";
+    std::string window_title = "2D Beagle";
+
+    // Size in bytes of structure. Must always be set to sizeof(WNDCLASSEX)
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WindowProc;
+    wcex.hInstance = hInstance;
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.lpszClassName = window_class_name.c_str();
+
+    if (!RegisterClassEx(&wcex))
+    {
+        std::cout << "Failed to register window class." << std::endl;
+        return 1;
+    }
+
+    // Create window using the window class
+    HWND hwnd = CreateWindowEx(
+        0,
+        window_class_name.c_str(),
+        window_title.c_str(),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    // Wait for user to press a key before closing the application
+    // and thus the console window.
+    std::cin.get();
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
